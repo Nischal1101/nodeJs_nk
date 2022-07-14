1cace3f8ff6c31dffbb20c01dbbaf475c5350d67
Nischal <kaflenk123@gmail.com>
2022-07-14 16:02:41 +0545

    masala

--- tangs.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hi this is Nischal" << endl;
-    cout << "hello please ignore this .cpp file" << endl;
-    cout << "hello";
-}
