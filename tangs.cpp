a201431da6f395217687d14250e45df65e70be68
Nischal <kaflenk123@gmail.com>
2022-07-14 15:34:20 +0545

    initialized git ignore

--- tangs.cpp
+++ tangs.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"Hi this is Nischal"<<endl;
+    cout << "Hi this is Nischal" << endl;
+    cout << "hello please ignore this .cpp file" << endl;
+    cout << "hello";
 }
